290930cb1f2a72a33bf3486703be64273daee2e0
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-06-08 00:08:28 +0200

    Update package.json to add homepage and deploy property

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, {Component} from 'react';
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-use-before-define */
+import React, { Component } from 'react';
 import Quote from './components/quote/quote';
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       dataURL: 'https://type.fit/api/quotes',
-    }
+    };
   }
 
-  render () {
+  render() {
     return (
       <div>
         <Quote dataURL={this.state.dataURL} />
